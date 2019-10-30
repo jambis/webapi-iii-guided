573f95784e3845bddb7ec0a7988d94c9c1c1b70d
jambis <imjaamees@gmail.com>
2019-10-30 12:01:42 -0500

    Method URL logger

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+function methodUrlLogger(req, res, next) {
+  console.log(`${req.method} method to the URL ${req.originalUrl}`);
+
+  next();
+}
+
+server.use(helmet());
 server.use(express.json());
+server.use(methodUrlLogger);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
